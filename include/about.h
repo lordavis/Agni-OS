45b8316586e7461a22ecf2df6d4ecdabf73d2983
Sivadev Smijith <sivasmijith@gmail.com>
2025-12-01 09:09:47 +0530

    Initial project commit

---
+++ include/about.h
@@ -0,0 +1,21 @@
+#ifndef __ABOUT_H__
+#define __ABOUT_H__
+
+#define OS_NAME "AGNI OS"
+#define VERSION "0.1"
+#define DEVELOPER "Team AGNI"
+#define OS_DESC "AGNI is a new operating system which is expected to overtake all other operating systems in quality and experience..."  
+void os_about(){
+print("\nOS NAME: ");
+print(OS_NAME);
+print("\nVersion: ");
+print(VERSION);
+print("\nDeveloper: ");
+print(DEVELOPER);
+print("\n");
+}
+
+void os_logo(){
+
+}
+#endif
